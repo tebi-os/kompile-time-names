407cb4e0253132b65b71d513eed3a280c30b8e74
Desmond van der Meer <desmond@tebi.co>
2025-10-03 13:28:09 +0200

    Add signing

--- buildSrc/src/main/kotlin/kompile-time-names-convention.gradle.kts
+++ buildSrc/src/main/kotlin/kompile-time-names-convention.gradle.kts
@@ -1,3 +1,14 @@
+plugins {
+    signing
+}
+
 version = BuildConfig.VERSION
 group = BuildConfig.GROUP
 description = BuildConfig.DESCRIPTION
+
+signing {
+    useInMemoryPgpKeys(
+        properties["tebi.signing.key"].toString(),
+        properties["tebi.signing.password"].toString(),
+    )
+}
