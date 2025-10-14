6925217c5ee1e3601691717e2a11245d24c45263
Desmond van der Meer <desmond@tebi.co>
2025-10-14 08:58:40 +0200

    Also allow new/unknown functions

--- compiler-plugin/src/main/kotlin/com/tebi/ktn/compiler/ir/WithKompileTimeNamesCallTransformer.kt
+++ compiler-plugin/src/main/kotlin/com/tebi/ktn/compiler/ir/WithKompileTimeNamesCallTransformer.kt
@@ -14,82 +14,133 @@ import org.jetbrains.kotlin.ir.types.classOrNull
 import org.jetbrains.kotlin.ir.types.classifierOrNull
 import org.jetbrains.kotlin.ir.util.SYNTHETIC_OFFSET
 import org.jetbrains.kotlin.ir.util.fqNameWhenAvailable
+import org.jetbrains.kotlin.ir.util.hasAnnotation
 import org.jetbrains.kotlin.ir.util.toIrConst
 import org.jetbrains.kotlin.ir.visitors.IrElementTransformerVoid
 
 
 class WithKompileTimeNamesCallTransformer(
     private val context: IrPluginContext,
-    private val transformedFunctions: Map<IrSimpleFunctionSymbol, List<GeneratedQualifiedNameParameter>>,
+    private val transformedFunctions: MutableMap<IrSimpleFunctionSymbol, List<GeneratedQualifiedNameParameter>>,
 ) : IrElementTransformerVoid() {
 
     private val kompileTimeQualifiedNameSymbol = context
         .referenceFunctions(KTNIDs.CallableIDs.KompileTimeQualifiedName)
         .singleOrNull()
-        ?: error("Unable to resolve symbol for ${KTNIDs.CallableIDs.KompileTimeQualifiedName}")
 
-    @OptIn(UnsafeDuringIrConstructionAPI::class)
     override fun visitCall(expression: IrCall): IrExpression {
         if (expression.symbol == kompileTimeQualifiedNameSymbol) {
-            return expression.typeArguments[0].resolveQualifiedName()
+            return substituteKompileTimeQualifiedName(expression)
         }
 
-        val additionalParameters = transformedFunctions[expression.symbol]
-            ?: return super.visitCall(expression)
+        transformedFunctions[expression.symbol]?.let { cachedAdditionalParameters ->
+            addAdditionalArguments(expression, cachedAdditionalParameters)
+            return super.visitCall(expression)
+        }
 
-        for (additionalParameter in additionalParameters) {
-            val typeArgument = expression.typeArguments[additionalParameter.typeParameter.index]
-            expression.arguments.add(typeArgument.resolveQualifiedName())
+        checkFunction(expression)?.let { additionalParameters ->
+            addAdditionalArguments(expression, additionalParameters)
+            return super.visitCall(expression)
         }
 
         return super.visitCall(expression)
     }
 
     @OptIn(UnsafeDuringIrConstructionAPI::class)
-    private fun IrType?.resolveQualifiedName(): IrExpression {
+    private fun checkFunction(expression: IrCall): List<GeneratedQualifiedNameParameter>? {
+        val declaration = expression.symbol.owner
+
+        if (declaration.parameters.size <= expression.arguments.size) {
+            return null
+        }
+
+        val annotatedTypeParameters = declaration.typeParameters
+            .filter { it.hasAnnotation(KTNIDs.ClassIDs.WithKompileTimeNames) }
+
+        if (declaration.parameters.size - expression.arguments.size != annotatedTypeParameters.size) {
+            // TODO: Diagnostics error
+            return null
+        }
+
+        val result = annotatedTypeParameters.mapIndexed { index, typeParameter ->
+            GeneratedQualifiedNameParameter(
+                typeParameter = typeParameter,
+                qualifiedNameParameter = declaration.parameters[declaration.parameters.size - expression.arguments.size + index],
+            )
+        }
+
+        transformedFunctions[expression.symbol] = result
+
+        return result
+    }
+
+    private fun substituteKompileTimeQualifiedName(expression: IrCall): IrExpression {
+        return expression.typeArguments[0].resolveQualifiedName(
+            startOffset = expression.startOffset,
+            endOffset = expression.endOffset,
+        )
+    }
+
+    private fun addAdditionalArguments(
+        expression: IrCall,
+        additionalParameters: List<GeneratedQualifiedNameParameter>,
+    ) {
+        for (additionalParameter in additionalParameters) {
+            val typeArgument = expression.typeArguments[additionalParameter.typeParameter.index]
+            expression.arguments.add(typeArgument.resolveQualifiedName())
+        }
+    }
+
+    @OptIn(UnsafeDuringIrConstructionAPI::class)
+    private fun IrType?.resolveQualifiedName(
+        startOffset: Int = SYNTHETIC_OFFSET,
+        endOffset: Int = SYNTHETIC_OFFSET,
+    ): IrExpression {
         if (this == null) {
             // TODO: Diagnostics error
-            return null.toIrConst()
+            return null.toIrConst(startOffset, endOffset)
         }
 
         val classSymbol = classOrNull
         if (classSymbol != null) {
-            return classSymbol.owner.fqNameWhenAvailable?.asString().toIrConst()
+            return classSymbol.owner.fqNameWhenAvailable?.asString().toIrConst(startOffset, endOffset)
         }
 
         val typeParameterSymbol = classifierOrNull as? IrTypeParameterSymbol
         if (typeParameterSymbol == null) {
             // TODO: Diagnostics error
-            return null.toIrConst()
+            return null.toIrConst(startOffset, endOffset)
         }
 
         val functionSymbol = (typeParameterSymbol.owner.parent as? IrSymbolOwner)?.symbol
         if (functionSymbol == null) {
             // TODO: Diagnostics error
-            return null.toIrConst()
+            return null.toIrConst(startOffset, endOffset)
         }
 
         val functionAdditionalParameters = transformedFunctions[functionSymbol]
         if (functionAdditionalParameters == null) {
             // TODO: Diagnostics error
-            return null.toIrConst()
+            return null.toIrConst(startOffset, endOffset)
         }
 
         val param = functionAdditionalParameters.firstOrNull { it.typeParameter.symbol == typeParameterSymbol }
         if (param == null) {
             // TODO: Diagnostics error
-            return null.toIrConst()
+            return null.toIrConst(startOffset, endOffset)
         }
 
         return IrGetValueImpl(
-            startOffset = SYNTHETIC_OFFSET,
-            endOffset = SYNTHETIC_OFFSET,
+            startOffset = startOffset,
+            endOffset = endOffset,
             symbol = param.qualifiedNameParameter.symbol,
         )
     }
 
-    private fun String?.toIrConst() = toIrConst(
+    private fun String?.toIrConst(startOffset: Int, endOffset: Int) = toIrConst(
         irType = if (this != null) context.irBuiltIns.stringType else context.irBuiltIns.nothingNType,
+        startOffset = startOffset,
+        endOffset = endOffset,
     )
 
 }
